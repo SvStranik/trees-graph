042f67f4e3f0dd8731a08db4001bb0bc5839a378
Svyatoslav <77211900+SvStranik@users.noreply.github.com>
2021-10-11 22:39:51 +0300

    Update GenerateBBSTArray.py

--- GenerateBBSTArray.py
+++ GenerateBBSTArray.py
@@ -1,12 +1,12 @@
 def GenerateBBSTArray(a):
     if len(a) < 1: return None
     array_sorted = sorted(a)
-    def ppp(array_sorted,final_array=[None] * len(a), I=0):
+    def recursivBBSTArray(array_sorted,final_array=[None] * len(a), I=0):
         central_element = len(array_sorted) // 2
         if not array_sorted == []:
             final_array[I] = array_sorted[central_element]
-            ppp(array_sorted[:central_element], final_array, 2*I+1)
-            ppp(array_sorted[central_element+1:], final_array, 2*I+2)
+            recursivBBSTArray(array_sorted[:central_element], final_array, 2*I+1)
+            recursivBBSTArray(array_sorted[central_element+1:], final_array, 2*I+2)
         return final_array
-    resultat = ppp(array_sorted)
+    resultat = recursivBBSTArray(array_sorted)
     return resultat
