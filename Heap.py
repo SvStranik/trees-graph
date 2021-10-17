55e4c13fc7e7a6495d1775f9259f9e14df9bb615
Svyatoslav <77211900+SvStranik@users.noreply.github.com>
2021-10-17 17:26:28 +0300

    Update Heap.py

--- Heap.py
+++ Heap.py
@@ -12,20 +12,19 @@ class Heap:
 
     def MakeHeap(self, a, depth):
         array_heap = self.CreateArrayHeap(depth)
-        [self.Add(element_heap) for element_heap in a]             
+        [self.Add(element_heap) for element_heap in a]
 
     def GetMax(self):
         array_heap = self.getHeapArray()
         try:
-            if not array_heap: return -1
+            if not array_heap or array_heap[0] == None : return -1
             position_el_add = array_heap.index(None) 
         except:
             position_el_add = len(array_heap)
+        root_value = array_heap[0]
         array_heap[0] = array_heap[position_el_add-1]
         array_heap[position_el_add-1] = None
         position_el_add = 0
-        if array_heap[0] == None: return -1
-        
         def DeletElementInHeap(array_heap,position_el_add):
             index_left_node = (len(array_heap) - 1 if 2*position_el_add + 1 >= len(array_heap) else 2*position_el_add + 1)
             index_right_node = (len(array_heap) - 1 if 2*position_el_add + 2 >= len(array_heap) else 2*position_el_add + 2)
@@ -40,9 +39,8 @@ class Heap:
                 position_el_add = max_index_node
             else: return
             DeletElementInHeap(array_heap,position_el_add)
-            
         DeletElementInHeap(array_heap,position_el_add)   
-        return (array_heap[0],array_heap)
+        return root_value
 
     def Add(self, key):
         array_heap = self.getHeapArray()
@@ -59,6 +57,5 @@ class Heap:
                 array_heap[position_el_add],array_heap[position_parent] = array_heap[position_parent],array_heap[position_el_add]
                 position_el_add = position_parent
             return InsertElementInHeap(array_heap,position_el_add,(position_el_add-1) //2)
-          
         array_heap = InsertElementInHeap(array_heap,position_el_add)
         return array_heap
