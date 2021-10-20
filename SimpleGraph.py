94e261ebf2e0cd5b36da510d70cc39f349e07b32
Svyatoslav <77211900+SvStranik@users.noreply.github.com>
2021-10-20 14:04:28 +0300

    Update SimpleGraph.py

--- SimpleGraph.py
+++ SimpleGraph.py
@@ -19,32 +19,38 @@ class SimpleGraph:
     def getObjectValueVertex(self,val):
         return Vertex(val)
         
+    def CheckingVertexes(self,v):
+        list_vertex = self.getListVertex()
+        try: return isinstance(list_vertex[v],Vertex)
+        except: return False
+            
     def AddVertex(self, v):
         list_vertex = self.getListVertex()
         for (i,j) in enumerate(list_vertex):
             if not j:
                 list_vertex[i] = self.getObjectValueVertex(v)
-                return list_vertex[i]
-    
+                return
+                
     def RemoveVertex(self, v):
         adjacency_matrix = self.getAdjacencyMatrix()
-        for i in range(self.max_vertex):
-            adjacency_matrix[v][i] = 0
-            adjacency_matrix[i][v] = 0
+        if self.CheckingVertexes(v):
+            for i in range(self.max_vertex):
+                adjacency_matrix[v][i] = 0
+                adjacency_matrix[i][v] = 0
     
     def IsEdge(self, v1, v2):
         adjacency_matrix = self.getAdjacencyMatrix()
-        return (True if adjacency_matrix[v1][v2] == 
-                        adjacency_matrix[v2][v1] == 1 else False)  
-    
+        if self.CheckingVertexes(v1) and self.CheckingVertexes(v2):
+            return (True if adjacency_matrix[v1][v2] == 
+                            adjacency_matrix[v2][v1] == 1 else False)
+        else: return False
+            
     def AddEdge(self, v1, v2):
         adjacency_matrix = self.getAdjacencyMatrix()
-        adjacency_matrix[v1][v2] = 1
-        adjacency_matrix[v2][v1] = 1
-    
+        if self.CheckingVertexes(v1) and self.CheckingVertexes(v2):
+            adjacency_matrix[v1][v2] = adjacency_matrix[v2][v1] =1
+   
     def RemoveEdge(self, v1, v2):
         adjacency_matrix = self.getAdjacencyMatrix()
-        adjacency_matrix[v1][v2] = 0
-        adjacency_matrix[v2][v1] = 0
-
-
+        if self.CheckingVertexes(v1) and self.CheckingVertexes(v2):
+            adjacency_matrix[v1][v2] = adjacency_matrix[v2][v1] = 0
